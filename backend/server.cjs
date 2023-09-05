2ff5a6e34f62fff08d0231c8d2577b022e657edc
ionut ciornei <Pantelimon194@gmail.com>
2023-09-05 21:06:56 +0300

    Udated the server

--- backend/server.cjs
+++ backend/server.cjs
@@ -3,15 +3,14 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 const path = require('path');
 
-console.log("Directory:", __dirname);
-
+// Environment Config
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
 if (!process.env.EMAIL || !process.env.PASSWORD) {
-    console.error("EMAIL and PASSWORD environment variables must be set!");
-    process.exit(1);
+  console.error("EMAIL and PASSWORD environment variables must be set!");
+  process.exit(1);
 }
 
 const app = express();
@@ -19,7 +18,15 @@ const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
-// Configure CORS
+// Logging Middleware for Debugging CORS
+app.use((req, res, next) => {
+  console.log('Request Origin:', req.get('origin'));
+  console.log('Request Method:', req.method);
+  console.log('Request Headers:', req.headers);
+  next();
+});
+
+// Configure and Enable CORS
 const corsOptions = {
   origin: process.env.CORS_ORIGIN || 'https://ionut1oo.github.io',
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
@@ -27,21 +34,16 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
-
-// Enable CORS
 app.use(cors(corsOptions));
 
-// Logging Middleware for Debugging CORS
-app.use((req, res, next) => {
-  console.log('CORS Headers:', res.getHeaders());
-  console.log('Request Origin:', req.get('origin'));
-  console.log('Request Method:', req.method);
-  console.log('Request Headers:', req.headers);
-  next();
-});
-
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+const buildPath = path.join(__dirname, 'client/build');
+if (path.resolve(__dirname, buildPath)) {
+  app.use(express.static(buildPath));
+} else {
+  console.error(`The directory ${buildPath} does not exist!`);
+  process.exit(1);
+}
 
 // Add a GET route for "/"
 app.get('/', (req, res) => {
@@ -50,12 +52,18 @@ app.get('/', (req, res) => {
 
 // Code for sending an email
 app.post("/api/sendEmail", async (req, res) => {
-
+  // Your email-sending code here
 });
 
 // Catch-all route to serve the React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));    
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 app.listen(port, () => {
