3a28e96fd4ab18bf62118c352e66089adc74e784
ionut ciornei <Pantelimon194@gmail.com>
2023-10-01 23:08:23 +0300

    changes on about me page

--- client/src/components/AboutMe.tsx
+++ client/src/components/AboutMe.tsx
@@ -9,7 +9,7 @@ const AboutMe:React.FC = () => {
          <div className="style-content"> 
             <h2 className="text-primary">About Me</h2>
             <p>Hi, I'm Ionut Ciornei! I'm transitioning from a career as a driver to pursue my passion for Front-End Development.</p>
-            <p>With skills in HTML, CSS, JavaScript, TypeScript, React, React-Boostrap, ReduxToolkit, and React, I aim to create user experiences that are both intuitive and dynamic.</p>
+            <p>With skills in HTML, CSS, JavaScript, TypeScript, React, Boostrap, ReduxToolkit, and Git, I aim to create user experiences that are both intuitive and dynamic.</p>
             <p>My background has instilled in me a keen attention to detail and a unique approach to problem-solving.</p>
             <p>I'm actively seeking opportunities to grow in this exciting new field.</p> 
          </div>
